8f8f4b32bca631677c9096bcdfcfd64cd788df6a
seth <nguyenduong8bc1@gmail.com>
2025-06-05 11:19:21 +0930

    Restructuring a bit to implement algo more easily

--- network_sim.c
+++ network_sim.c
@@ -10,12 +10,13 @@ int DEBUG = 1;
 void DistanceVector(struct router list_input[DEFAULT_NETWORK_SIZE]);
 
 /*Returns the first index where an empty element is detected in the provided linkTable[]*/
-int findEmptyLink(struct link *table_input);
+int findLink(struct link *table_input);
 
 int main(int agrc , char **argv) {
 
     char user_input[DEFAULT_STR_SIZE];
     struct router router_list[DEFAULT_NETWORK_SIZE];
+    char input_list[DEFAULT_NETWORK_SIZE][DEFAULT_STR_SIZE];
 
     // 
     do {
@@ -33,6 +34,7 @@ int main(int agrc , char **argv) {
                 
                 /*If found an empty spot, put router in there*/
                 strcpy(router_list[i].router_name , user_input);
+                strcpy(input_list[i] , user_input);
 
                 /*Printing value of router_list[i] just to make sure*/
                 if(DEBUG) {
@@ -47,63 +49,79 @@ int main(int agrc , char **argv) {
     /*Will change this to START later*/
 } while (strcmp(user_input,"START"));
 
-    /*Process inputs for links*/
-    do {
-        int i;
-        printf("Enter router links: \n");
-        fgets(user_input , DEFAULT_STR_SIZE , stdin);
-        /*Remove \n char from string*/
-        user_input[strcspn(user_input , "\n")] = 0;
-        
-        if(user_input == "END") {
-            break;
-        };
-        
-        char* tokenised;
-        
-        /*Tokenise the inputs for processing*/
-        tokenised = strtok(user_input , " ");
-        
-        if(!strcmp(user_input,"testPrint")) {
-            for(i=0 ; i<DEFAULT_NETWORK_SIZE ; i++) {
-                if(strcmp(router_list[i].router_name , "")) {
-                    printf("Name %d: %s\n" , i , router_list[i].router_name);
-                }
-            }
-            continue;
-        }
-        
-        /*Set distance to self to 0*/
-        for(int i=0 ; i<DEFAULT_NETWORK_SIZE ; i++) {
-            strcpy(router_list[i].link_table[0].next_hop , router_list[i].router_name);
-            strcpy(router_list[i].link_table[0].destination , router_list[i].router_name);
-            router_list[i].link_table[0].distance_to_dest = 0;
-        }
-        
-        /*Input the neighboring links*/
-        for(i=0 ; i<DEFAULT_NETWORK_SIZE ; i++) {
-            if(!strcmp(router_list[i].router_name , tokenised));
-            {
-                    int empty_index = findEmptyLink(router_list[i].link_table);
-                    
-                    /* int empty_index = 0; using this for testing, remove later*/
-                    
-                    tokenised = strtok(NULL , " ");
-                    strcpy(router_list[i].link_table[empty_index].destination , tokenised);
-                    strcpy(router_list[i].link_table[empty_index].next_hop , tokenised);
+/*Make new and blank router connections for each routers*/
+for(int i=0 ; i<DEFAULT_NETWORK_SIZE ; i++) {
+    for(int j=0 ; j<DEFAULT_NETWORK_SIZE ; j++) {
+        /*If found 2 different router then set cost to inf*/
+        if(!strcmp(router_list[i].router_name , router_list[j].router_name)) {
+            strcpy(router_list[i].link_table[j].destination , router_list[j].router_name);
+            router_list[i].link_table[j].distance_to_dest = -1;       
 
-                    tokenised = strtok(NULL , " ");
-                    int to_int = *tokenised - '0';
-                    router_list[i].link_table[empty_index].distance_to_dest = to_int;
-                    break;
-                }
+        } else {
+            /*If found 2 router of same name then set cost to 0*/
+            strcpy(router_list[i].link_table[j].destination , router_list[i].router_name);
+            strcpy(router_list[i].link_table[j].next_hop , router_list[j].router_name);
+            router_list[i].link_table[j].distance_to_dest = 0;
+
         }
-        
-        if(DEBUG) {
-            printf("Info: \nRouter: %s\nLink %s - %d\n  " , router_list[0].router_name , router_list[0].link_table[0].destination , router_list[0].link_table[0].distance_to_dest);
+    }
+}
+
+/*Printing the link table of each router*/
+
+for(int i=0 ; i<DEFAULT_NETWORK_SIZE ; i++) {
+    printf("Router %s link table\n" , router_list[i].router_name);
+    printf("\n");
+
+    for(int j=0 ; j<DEFAULT_NETWORK_SIZE ; j++) {
+        printf("dest: %s\t hop: %s\t cost: %d\n" , 
+        router_list[i].link_table[j].destination , router_list[i].link_table[j].next_hop , 
+        router_list[i].link_table[j].distance_to_dest);
+    }
+}
+
+
+
+/*Process inputs for links*/
+do {
+    int i;
+    printf("Enter router links: \n");
+    fgets(user_input , DEFAULT_STR_SIZE , stdin);
+    /*Remove \n char from string*/
+    user_input[strcspn(user_input , "\n")] = 0;
+    
+    if(user_input == "END") {
+        break;
+    };
+    
+    char* tokenised;
+    
+    /*Tokenise the inputs for processing*/
+    tokenised = strtok(user_input , " ");
+    
+    if(!strcmp(user_input,"testPrint")) {
+        for(i=0 ; i<DEFAULT_NETWORK_SIZE ; i++) {
+            if(strcmp(router_list[i].router_name , "")) {
+                printf("Name %d: %s\n" , i , router_list[i].router_name);
+            }
         }
-        
-    } while(strcmp(user_input , "END"));
+        continue;
+    }
+    
+    
+    /*Input the neighboring links*/
+    for(i=0 ; i<DEFAULT_NETWORK_SIZE ; i++) {
+        if(!strcmp(router_list[i].router_name , tokenised)) {
+
+
+        }
+    }
+    
+    if(DEBUG) {
+        printf("Info: \nRouter: %s\nLink %s - %d\n  " , router_list[0].router_name , router_list[0].link_table[0].destination , router_list[0].link_table[0].distance_to_dest);
+    }
+    
+} while(strcmp(user_input , "END"));
 
     printf("Calculating Distance matrix\n");
     
@@ -123,7 +141,7 @@ void DistanceVector(struct router list_input[DEFAULT_NETWORK_SIZE]) {
 
 
 
-int findEmptyLink(struct link *table_input) {
+int findLink(struct link *table_input) {
     int i;
     for(i=0 ; i<DEFAULT_TABLE_SIZE ; i++) {
         if(*table_input[i].destination == '\0') {
@@ -131,6 +149,6 @@ int findEmptyLink(struct link *table_input) {
         }
     }
 
-    printf("From findEmptyLink(): No empty indexes found\n");
+    printf("From findLink(): No empty indexes found\n");
     return -1;
 };
